2ec0d61b19e780c83f1bc5fa810bbe479f570c06
tom-mats <dawn.gray.sky@gmail.com>
2016-04-12 01:12:51 +0900

    fix to submit

--- app/controllers/iss_controller.rb
+++ app/controllers/iss_controller.rb
@@ -1,23 +1,16 @@
 class IssController < ApplicationController
   unloadable
-  class ExtractForm
-    include ActiveModel::Model
-
-    attr_accessor :ip, :issue, :id
-  end
 
 
   def index
-    @form = ExtractForm.new
-    @form.issue = params[:issue]
-    @form.id = params[:id]
+    @issue = params[:issue]
+    @id = params[:id]
   end
 
   def new
   end
 
   def req
-    p @form
     p params
     redirect_to :back
   end
