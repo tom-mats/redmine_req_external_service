dd2ca6de106432b6b547798917e8fcf6c63d4732
tom-mats <dawn.gray.sky@gmail.com>
2016-04-12 10:39:39 +0900

    delete unused files

--- app/controllers/iss_controller.rb
+++ app/controllers/iss_controller.rb
@@ -1,22 +1,26 @@
 class IssController < ApplicationController
+  unloadable
+  
   class IssConfig
     include ActiveModel::Model
 
     attr_accessor :id, :issue, :ip
   end
   def index
+    @form = params[:notice]
     @form = IssConfig.new
     @form.issue = params[:issue]
     @form.id = params[:id]
-    list = User.current.custom_field_values.select{|elem| elem.custom_file.name == "list"}.map { |e| e.value }
-  end
-
-  def new
+    list = User.current.custom_field_values
+             .select{|e| e.custom_field.name == "list"}
+             .map { |e| e.value.split(",") }.flatten
+             .map { |e| e.strip }
+    ary = [list,list].transpose
+    @ip_table = Hash[*ary.flatten]
   end
 
   def req
-    p params
-    p @form
-    redirect_to :back
+    ip = params[:iss_controller_iss_config][:ip]
+    redirect_to ({action: 'index', issue: params[:issue]}), notice: "Extract to #{ip}. Wait a minute..."
   end
 end
--- app/controllers/zip_controller.rb
+++ app/controllers/zip_controller.rb
@@ -1,13 +1,25 @@
 class ZipController < ApplicationController
   unloadable
+  class ZipConfig
+    include ActiveModel::Model
 
-
-  def index
+    attr_accessor :id, :issue, :ip
   end
-
-  def new
+  def index
+    @form = params[:notice]
+    @form = IssConfig.new
+    @form.issue = params[:issue]
+    @form.id = params[:id]
+    list = User.current.custom_field_values
+             .select{|e| e.custom_field.name == "list"}
+             .map { |e| e.value.split(",") }.flatten
+             .map { |e| e.strip }
+    ary = [list,list].transpose
+    @ip_table = Hash[*ary.flatten]
   end
 
   def req
+    ip = params[:iss_controller_iss_config][:ip]
+    redirect_to ({action: 'index', issue: params[:issue]}), notice: "Extract to #{ip}. Wait a minute..."
   end
 end
